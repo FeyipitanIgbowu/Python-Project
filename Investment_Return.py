2e7e17764a240fc8135ed11dca6ed83463a852cf
Feyipitan Igbowu <feyiigbowu@gmail.com>
2025-07-15 11:18:20 +0100

    Python

---
+++ Investment_Return.py
@@ -0,0 +1,12 @@
+r = 7
+n1 = 10
+n2 = 20
+n3 = 30
+p = 1000
+
+a1 = 1000 * (r ** 10)
+print("After 10 years", a1)
+a2 = 1000 * (r ** 20)
+print("After 20 years", a2)
+a3 = 1000 * (r **30)
+print("After 30 years", a3)
---
+++ Multiples.py
@@ -0,0 +1,13 @@
+number1 = 1024
+number2 = 10
+
+if number1 % 4 == 0
+print("It is a multiple of 4")
+else
+print("It is not a multiple of 4")
+
+
+if number2 % 2 == 0
+print("It is a multiple of 2")
+else
+print("It is not a multiple of 2")
